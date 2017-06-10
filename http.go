165a6bc54c15cdb36bc82c9f67a352ae440ab50b
Zuolar <yam8511@gmail.com>
2017-06-10 22:04:16 +0800

    客製化Port

--- http.go
+++ http.go
@@ -3,6 +3,7 @@ package main
 import (
 	// "database/sql"
 	"fmt"
+	"strings"
 	// _ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
@@ -22,21 +23,22 @@ type App struct {
 var Zuzu App
 
 func main() {
+	Zuzu.startServe(8000)
+}
+
+func (app App) startServe(port uint) {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Println("Error:", err)
 		}
 	}()
 
-	Zuzu.startServe()
-}
-
-func (app App) startServe() {
 	// 當瀏覽器輸入根目錄時會呼叫 indexHandle() 涵式
 	// 當 Request 找不到指定 url 會選擇根目錄
 	// 若連 根目錄 也找不到, 會跑出 "404 page not found"
 	http.HandleFunc("/", serverHandler)
-	http.ListenAndServe(":8000", nil)
+	listenPort := fmt.Sprintf(":%d", port)
+	http.ListenAndServe(listenPort, nil)
 }
 
 func serverHandler(res http.ResponseWriter, req *http.Request) {
@@ -64,7 +66,22 @@ func (app App) Start() {
 	handler, exists := route[currentURL]
 
 	if !exists {
-		app.view("404", nil)
+		fmt.Println("Header", app.req.self.Header)
+		headerAccept, headerExists := app.req.self.Header["Accept"]
+		substr := "image"
+		needResponse := true
+		if headerExists {
+			for _, acceptItem := range headerAccept {
+				if strings.Contains(acceptItem, substr) {
+					needResponse = false
+					break
+				}
+			}
+		}
+
+		if needResponse {
+			app.view("404", nil)
+		}
 	} else {
 		handler(&app)
 	}
