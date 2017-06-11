a37f922cc7149614b872db7c7b39de710a03474c
Zuolar <yam8511@gmail.com>
2017-06-12 00:40:14 +0800

    heroku

--- demo.go
+++ demo.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	ZuZuGo "github.com/yam8511/ZuZuGo"
 )
@@ -9,12 +12,19 @@ import (
 var app *ZuZuGo.App
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	fmt.Println("Hello World")
 	app = new(ZuZuGo.App)
 	route := map[string]func(*ZuZuGo.App){}
 	route["/"] = indexHandler
 	app.SetRoute(route)
-	app.StartServe(8000)
+	p, _ := strconv.Atoi(port)
+	app.StartServe(uint(p))
 }
 
 func indexHandler(app *ZuZuGo.App) {
